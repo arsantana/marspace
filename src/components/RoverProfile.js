352d47e8f6ffcf05139979d6ed875a8b59f0297f
Arleen Santana <santana.arleen@gmail.com>
2021-05-06 18:26:29 -0400

    added rover profile - card

--- src/components/RoverProfile.js
+++ src/components/RoverProfile.js
@@ -2,6 +2,12 @@ import React from "react";
 
 export default function RoverProfile() {
     return(
-        <div>Rover Profile</div>
+        <div className="roverCard">
+            <h1 className="roverName">Insight</h1>
+            <div className="roverImage"></div>
+            <h3 className="roverLocation">Location:</h3>
+            <h3 className="roverLastActive">Last Active:</h3>
+            <p className="roverBio">Interior Exploration using Seismic Investigations, Geodesy and Heat Transport, but you can call me InSight!</p>
+        </div>
     )
 }
