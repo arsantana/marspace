06addbf3123ff3f34692b9193968ca5a4da43051
Arleen Santana <santana.arleen@gmail.com>
2021-05-06 17:33:15 -0400

    added navbar

---
+++ src/components/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+export default function NavBar() {
+    return(
+      <div className="navBar">
+    <div className="logoBox">
+      <h1 className="logoBox--text">Marspace</h1>
+      <div className="logoBox--img"></div>
+    </div>
+  </div>
+    )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,15 @@
-import React from "react"
+import React from "react";
+import '../css/styles.css';
+
+import RoverProfile from "../components/RoverProfile";
+import NavBar from "../components/NavBar";
+
 
 export default function Home() {
-  return <div>Hello world!</div>
+   return(
+    <div className="index">
+      <NavBar />
+      <RoverProfile />
+   </div>
+   ) 
 }
